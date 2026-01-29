3179694c4e1dc3abb11b6894da3911617f1e9ec3
JoseManuel <joseenriquezbaena@gmail.com>
2026-01-29 16:33:00 +0100

    feat: Configuracion minima MQTT, publicacion y subscripcion topicos telemetria

--- firmware/esp32-circuit/main/main.c
+++ firmware/esp32-circuit/main/main.c
@@ -10,9 +10,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <time.h>
 #include "driver/gpio.h"
+#include "mqtt_client.h"
 #include "esp_log.h"
 #include "DHT11.h"
+#include "frozen.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
@@ -29,10 +32,18 @@
 #define OFF_BUTTON GPIO_NUM_27
 #define DHT11_SENSOR GPIO_NUM_14
 #define ESP_INTR_FLAG_DEFAULT 0
+#define ID 1
 
 #define LOW 0
 #define HIGH 1
 
+/**
+ *  @brief Estados principales del dispositivo
+ *  
+ *  - Performance: Lectura periodica de los sensores y envio al broker MQTT
+ *  - configuration:
+ *  - off: No se realiza lecturas del sensor el dispositivo (modo sleep)
+ */
 typedef enum
 {
     performance,
@@ -40,6 +51,9 @@ typedef enum
     off
 } State_t;
 
+/**
+ * @brief Estructura de datos que almacena los datos leidos por los sensores
+ */
 typedef struct{
     uint8_t humicity;
     uint8_t temperature;
@@ -48,17 +62,30 @@ typedef struct{
 
 static QueueHandle_t isr_handler_queue = NULL;
 static State_t currentState = performance;
+esp_mqtt_client_handle_t client; // client debe ser global para poder publicar desde publish_data()
+
+// Etiquetas para la funcion ESP_LOG()
 static const char* TAG_SENSOR = "SENSOR_TASK";
+static const char* TAG_MQTT = "MQTT";
+
+// Las variables se definen en un archivo privado por seguridad.
+static const char* broker_uri = CONFIG_MQTT_BROKER_URI;
+static const char* username = CONFIG_USERNAME;
+static const char* password = CONFIG_PASSWORD;
 
 /**
  * -------------------------------------------------
  * PROTOTIPO DE FUNCIONES
  * -------------------------------------------------
  */
-void debug_io(uint64_t io);
-void set_io_level(uint32_t level_red, uint32_t level_yellow, uint32_t level_green);
-esp_err_t button_config();
-esp_err_t leds_config();
+static void debug_io(uint64_t io);
+static void set_io_level(uint32_t level_red, uint32_t level_yellow, uint32_t level_green);
+static esp_err_t button_config();
+static esp_err_t leds_config();
+static void publish_data(data_t* data, const char* topic);
+static time_t get_timestamp();
+static void mqtt_app_start();
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 
 /**
  * -------------------------------------------------
@@ -136,7 +163,7 @@ void vReadSensorTask(void* pvParameters)
                 data.humicity = humicity_int;
                 data.temperature = temperature_int;
                 ESP_LOGI(TAG_SENSOR, "Lectura de humedad: %d | Lectura de temperatura: %d", data.humicity, data.temperature);
-                // Despertar la tarea de mandar datos por MQTT mandando la estructura data 
+                
             }else{
                 switch (err)
                 {
@@ -190,13 +217,22 @@ void app_main(void)
     leds_config();
     button_config();
     dht11_init(DHT11_SENSOR);
-    isr_handler_queue = xQueueCreate(10, sizeof(uint32_t)); // Crear cola para manejar interrupcion
-
+    isr_handler_queue = xQueueCreate(10, sizeof(uint32_t)); 
+    mqtt_app_start();
+    
     // ------ CREATION TASKS ------
-    xTaskCreate(vControlFSMTask,"FSM Control Task", 2048, NULL, 6, NULL); // Tarea que controla los estados del fsm
-    xTaskCreate(vReadSensorTask,"Read Sensor Task", 4096, NULL, 6, NULL); // Tarea que dependiendo del modo, lee el sensor o no
-    xTaskCreate(vChangeStateTask,"Change State Task", 4096, NULL, 7, NULL); // Tarea que la isr despierta para cambiar de estado
-    // xTaskCreate(vMonitorTask,"Monitor Task",4096, NULL, , NULL);// Tarea para DEBUG
+
+    // Gestiona los estados de la FSM
+    xTaskCreate(vControlFSMTask,"FSM Control Task", 2048, NULL, 6, NULL); 
+    
+    // Lee los sensores en modo performance.
+    xTaskCreate(vReadSensorTask,"Read Sensor Task", 4096, NULL, 6, NULL); 
+    
+    // Despertada por la ISR para cambiar de estado. Prioridad maxima para respuesta inmediata
+    xTaskCreate(vChangeStateTask,"Change State Task", 4096, NULL, 7, NULL);
+    
+    // Tarea necesaria para hacer debug de los estados y caracteristicas de todas las tareas del sistema
+    // xTaskCreate(vMonitorTask,"Monitor Task",4096, NULL, , NULL);
 }
 
 /**
@@ -204,12 +240,12 @@ void app_main(void)
  * DEFINICION DE FUNCIONES
  * -------------------------------------------------
  */
-void debug_io(uint64_t io)
+static void debug_io(uint64_t io)
 {
     gpio_dump_io_configuration(stdout, io);
 }
 
-esp_err_t leds_config()
+static esp_err_t leds_config()
 {
     // Configuracion de pines
     gpio_config_t out_pin = {};
@@ -221,7 +257,7 @@ esp_err_t leds_config()
     return gpio_config(&out_pin);
 }
 
-esp_err_t button_config()
+static esp_err_t button_config()
 {
     // Configuracion de pines
     esp_err_t err;
@@ -241,9 +277,85 @@ esp_err_t button_config()
     return err;
 }
 
-void set_io_level(uint32_t level_red, uint32_t level_yellow, uint32_t level_green)
+static void set_io_level(uint32_t level_red, uint32_t level_yellow, uint32_t level_green)
 {
     gpio_set_level(LED_RED, level_red);
     gpio_set_level(LED_YELLOW, level_yellow);
     gpio_set_level(LED_GREEN, level_green);
+}
+
+static time_t get_timestamp(){
+    time_t now;
+    time(&now); 
+    return now;
+}
+
+static void publish_data(data_t* data){
+    char buffer[128];
+    struct json_out out = JSON_OUT_BUF(buffer, sizeof(buffer));
+    json_printf(&out, "{id: %d, temperature: %d, unidad: %s}", ID, data->temperature, "Celsius");
+    esp_mqtt_client_publish(client, "ESP32/1/telemetry/temperature", buffer, 0, 0, 0);
+    
+    out = JSON_OUT_BUF(buffer, sizeof(buffer));
+    json_printf(&out, "{id: %d, humidicity: %d, unidad: %s}", ID, data->humicity, "percentage");
+    esp_mqtt_client_publish(client, "ESP32/1/telemetry/humidicity", buffer, 0, 0, 0);
+
+    out = JSON_OUT_BUF(buffer, sizeof(buffer));
+    json_printf(&out, "{id: %d, light: %d, unidad: %s}", ID, data->light, "bool");
+    esp_mqtt_client_publish(client, "ESP32/1/telemetry/light", buffer, 0, 0, 0);
+}
+
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    // esp_mqtt_event_handle_t es una macro que es un puntero a esp_mqtt_event_t (estructura con los diferentes campos)
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED: 
+        ESP_LOGI(TAG_MQTT, "Dispositivo conectado correctamente\n");
+        
+        msg_id = esp_mqtt_client_subscribe(client, "ESP32/1/telemetry/temperature", 0);
+        ESP_LOGI(TAG_MQTT, "Suscripcion realizada, msg_id=%d\n", msg_id);
+        
+        msg_id = esp_mqtt_client_subscribe(client, "ESP32/1/telemetry/humidicity", 0);
+        ESP_LOGI(TAG_MQTT, "Suscripcion realizada, msg_id=%d\n", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "ESP32/1/telemetry/light", 0);
+        ESP_LOGI(TAG_MQTT, "Suscripcion realizada, msg_id=%d\n", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG_MQTT, "Dispositivo desconectado\n");
+        break;
+     case MQTT_EVENT_PUBLISHED:
+        ESP_LOGE(TAG_MQTT, "Suscripcion aceptada, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGE(TAG_MQTT, "Suscripcion rechazada, msg_id=%d\n", event->msg_id);
+        break;
+    default:
+        ESP_LOGI(TAG_MQTT, "Otro evento id:%d", event->event_id);
+        break;
+    }
+}
+
+static void mqtt_app_start()
+{
+    /*
+        No se configura id_cliente porque usa por defecto: ESP32_CHIPID% donde CHIPID% son los
+        ultimos 3 bytes(hex) de la MAC.
+        
+        !! OJO: Por ahora dejar configuracion por defecto los campos network_t, buffer_t, session_t, topic_t
+        session_t y topic_t es para Lass_will o ultima voluntad
+    */
+    esp_mqtt_client_config_t mqtt_conf = {
+        .broker.address.uri = broker_uri,
+        .credentials.username = username,
+        .credentials.authentication.password = password
+    };
+    client = esp_mqtt_client_init(&mqtt_conf);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
 }
