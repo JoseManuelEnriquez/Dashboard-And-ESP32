baa78099ecb8ef3387f09a703859dde006345fe5
JoseManuel <joseenriquezbaena@gmail.com>
2026-02-07 22:28:09 +0100

    feat: Agregar modulo de led

--- firmware/esp32-circuit/components/Leds/include/leds.h
+++ firmware/esp32-circuit/components/Leds/include/leds.h
@@ -1,10 +1,16 @@
 #ifndef LEDS_H
 #define LEDS_H
 #include "GPIO.h"
+#include "esp_driver_gpio"
+#include "board_definition.h"
 
 typedef struct{
     LED_OK,
     LED_ERR_INVALID
 }led_err_t
+
+led_err_t led_init();
+void led_on(uint32_t pin);
+void led_off(uint32_t pin);
 
 #endif
--- firmware/esp32-circuit/main/main.c
+++ firmware/esp32-circuit/main/main.c
@@ -19,24 +19,6 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
-/**
- * -----------------------------------------------------------
- * DEFINICIONES MACROS | VARIABLES GLOBALES | TIPOS DE DATOS
- * -----------------------------------------------------------
- */
-
-#define ID 1  
-
-#define LED_RED GPIO_NUM_23
-#define LED_GREEN GPIO_NUM_21
-#define LED_YELLOW GPIO_NUM_22
-#define CONNECTED_LED GPIO_NUM_17
-#define CONFIGURATION_LED GPIO_NUM_25
-#define CHANGE_BUTTON GPIO_NUM_26
-#define OFF_BUTTON GPIO_NUM_27
-#define DHT11_SENSOR GPIO_NUM_14
-#define LDR_SENSOR GPIO_NUM_19
-
 /*
 Hay que definir un delay minimo para la tarea ReadSensor porque el sensor DHT11 no puede hacer dos lecturas consecutivas
 sin haber pasado mas de 2 segundos. Como el delay se puede configurar, hay que controlar que no configuren un delay menor 
@@ -44,7 +26,7 @@ a 2 segundos
 */
 #define MIN_DELAY 2000
 
-#define ESP_INTR_FLAG_DEFAULT 0
+
 
 
 /**
