149947181e1d4df7b1569ad131b10964e8623404
JoseManuel <joseenriquezbaena@gmail.com>
2026-02-07 18:55:14 +0100

    feat: Modulo wifi

---
+++ firmware/esp32-circuit/components/Wifi/include/wifi.h
@@ -0,0 +1,23 @@
+#ifndef WIFI_H
+#define WIFI_H
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#define CONFIG_ESP_MAXIMUM_RETRY 3
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+EventGroupHandle_t s_wifi_event_group;
+const char* TAG_WIFI = "WIFI";
+static int s_retry_num;
+
+void wifi_init_sta();
+void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
+
+#endif
--- firmware/esp32-circuit/main/main.c
+++ firmware/esp32-circuit/main/main.c
@@ -12,16 +12,12 @@
 #include <inttypes.h>
 #include "frozen.h" // Libreria necesaria para crear json strings
 #include "DHT11.h"
+#include "wifi.h"
 #include "driver/gpio.h"
 #include "mqtt_client.h"
 #include "esp_log.h"
 #include "esp_event.h"
 #include "esp_system.h"
-#include "esp_log.h"
-#include "esp_wifi.h"
-#include "nvs_flash.h"
-#include "lwip/err.h"
-#include "lwip/sys.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
@@ -53,8 +49,6 @@ a 2 segundos
 
 #define ESP_INTR_FLAG_DEFAULT 0
 
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
 #define CONFIG_ESP_MAXIMUM_RETRY 3
 
 #define LOW 0
@@ -86,8 +80,6 @@ typedef struct{
 static QueueHandle_t isr_handler_queue = NULL; // Para despertar la tarea ChangeState
 static State_t currentState = idle; 
 esp_mqtt_client_handle_t client; // client debe ser global para poder publicar desde publish_data()
-static EventGroupHandle_t s_wifi_event_group;
-static int s_retry_num = 0;
 static int mqtt_connected = 0;
 static int delay = MIN_DELAY; // Tiempo que se queda bloqueada la tarea ReadSensor (es configurable por MQTT)
 
@@ -95,7 +87,6 @@ static int delay = MIN_DELAY; // Tiempo que se queda bloqueada la tarea ReadSens
 static const char* TAG_SENSOR = "SENSOR_TASK";
 static const char* TAG_CONFIG = "CONFIG";
 static const char* TAG_MQTT = "MQTT";
-static const char* TAG_WIFI = "WIFI";
 
 // Las variables se definen en un archivo privado por seguridad.
 static const char* broker_uri = CONFIG_MQTT_BROKER_URI;
@@ -115,8 +106,7 @@ static esp_err_t ldr_config();
 static void publish_data(data_t* data);
 static void mqtt_app_start();
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
-static void wifi_init_sta();
-static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
+
 
 /**
  * -------------------------------------------------
@@ -220,7 +210,7 @@ void vReadSensorTask(void* pvParameters)
 
 /**
  * -------------------------------------------------
- * FUNCIONES INTERRUPCIONES
+ * FUNCIONES INTEstatic RRUPCIONES
  * -------------------------------------------------
  */
 static void IRAM_ATTR gpio_isr_change_button_handler(void* args) // Manejador de interrupcion
@@ -482,63 +472,3 @@ static void mqtt_app_start()
     ESP_LOGI(TAG_MQTT,"APP MQTT START\n");
 }
 
-static void wifi_init_sta()
-{
-    s_wifi_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, &instance_got_ip));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = CONFIG_WIFI_SSID,
-            .password = CONFIG_WIFI_PASS,
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG_WIFI, "WIFI_INIT_STA FINISHED");
-
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
-
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG_WIFI, "CONNECTED");
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGE(TAG_WIFI, "CONNECTED FAILED");
-    } else {
-        ESP_LOGE(TAG_WIFI, "NO EXPECTED EVENT");
-    }
-}
-
-static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG_WIFI, "RETRY CONNECTION");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGE(TAG_WIFI,"CONECTION AP FAILED");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG_WIFI, "ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-    }
-}
