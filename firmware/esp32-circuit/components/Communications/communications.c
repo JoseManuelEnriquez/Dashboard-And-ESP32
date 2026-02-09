def380039509d3b19114054f4329b24a82a52d2c
JoseManuel <joseenriquezbaena@gmail.com>
2026-02-09 13:53:22 +0100

    feat: Implementacion mas robusta y con encapsulamiento de el modulo de comunicaciones

--- firmware/esp32-circuit/components/Communications/communications.c
+++ firmware/esp32-circuit/components/Communications/communications.c
@@ -14,16 +14,17 @@ typedef struct{
 }mqtt_topics_t;
 
 static mqtt_topics_t gTopics;
-static int id_device;
-static mqtt_callback callback_private;
 static esp_mqtt_client_handle_t client; // client debe ser global para poder publicar desde publish_data()
+
+static int id_device;
+static comm_callback callback_private;
 
 const static char* TAG_MQTT = "MQTT";
 const static char* broker_uri = CONFIG_BROKER_URI;
 const static char* username = CONFIG_USERNAME;
 const static char* password = CONFIG_PASSWORD;
 
-void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     // esp_mqtt_event_handle_t es una macro que es un puntero a esp_mqtt_event_t (estructura con los diferentes campos)
     esp_mqtt_event_handle_t event = event_data;
@@ -34,10 +35,10 @@ void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event
         ESP_LOGI(TAG_MQTT, "MQTT_EVENT_BIDEFORE_CONNECT");
         break;
     case MQTT_EVENT_CONNECTED: 
-        msg_id = esp_mqtt_client_subscribe(client, "ESP32/1/config/ON", 0);
-        msg_id = esp_mqtt_client_subscribe(client, "ESP32/1/config/SLEEP", 0);
-        msg_id = esp_mqtt_client_subscribe(client, "ESP32/1/config/CONFIG", 0);
-        msg_id = esp_mqtt_client_subscribe(client, "ESP32/1/config/delay", 0);
+        msg_id = esp_mqtt_client_subscribe(client, gTopics.on_topic, 0);
+        msg_id = esp_mqtt_client_subscribe(client, gTopics.sleep_topic, 0);
+        msg_id = esp_mqtt_client_subscribe(client, gTopics.config_topic, 0);
+        msg_id = esp_mqtt_client_subscribe(client, gTopics.delay_topic, 0);
         ESP_LOGI(TAG_MQTT, "MQTT_EVENT_CONNECTED");
         break;
     case MQTT_EVENT_DISCONNECTED:
@@ -71,29 +72,28 @@ void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event
                 Si value < MIN_DELAY, salta un error "INCORRECT DELAY"
         */
         ESP_LOGI(TAG_MQTT, "TOPIC: %.*s", event->topic_len, event->topic);
-        mqtt_message_t message;
+        comm_message_t message;
         if(strcmp(topic, gTopics.on_topic) == 0)
         {
-            message.topic = MQTT_ON;
-            message.status = MQTT_OK;
+            message.message_type = ON;
+            message.status = COMM_OK;
             callback_private(message);
         }else if(strcmp(topic, gTopics.config_topic) == 0)
         {
-            message.topic = MQTT_CONFIG;
-            message.status = MQTT_OK;
+            message.message_type = CONFIG;
+            message.status = COMM_OK;
             callback_private(message);
         }else if(strcmp(topic, gTopics.sleep_topic) == 0)
         {
-            message.topic = MQTT_SLEEP;
-            message.status = MQTT_OK;
+            message.message_type = SLEEP;
+            message.status = COMM_OK;
             callback_private(message);
         }else if(strcmp(topic, gTopics.delay_topic) == 0)
         {
-            message.topic = MQTT_DELAY;
-            message.status = MQTT_OK;
+            message.message_type = DELAY;
+            message.status = COMM_OK;
             message.data = event->data;
             callback_private(message);
-          
         }
         break;
     case MQTT_EVENT_ERROR:
@@ -105,15 +105,15 @@ void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event
     }
 }
 
-void mqtt_app_start(mqtt_callback callback, char device[MAX_LEN_DEVICE], int id)
+void comm_init(comm_callback callback, char device[MAX_LEN_DEVICE], int id)
 {
     callback_private = callback;
     id_device = id;
 
-    sprintf(gTopics.on_topic, MAX_LEN_TOPIC, "%s/%d/ON", device, id);
-    sprintf(gTopics.sleep_topic, MAX_LEN_TOPIC, "%s/%d/SLEEP", device, id);
-    sprintf(gTopics.config_topic, MAX_LEN_TOPIC, "%s/%d/CONFIG", device, id);
-    sprintf(gTopics.delay_topic, MAX_LEN_TOPIC, "%s/%d/DELAY", device, id);
+    sprintf(gTopics.on_topic, MAX_LEN_TOPIC, "%s/%d/config/ON", device, id);
+    sprintf(gTopics.sleep_topic, MAX_LEN_TOPIC, "%s/%d/config/SLEEP", device, id);
+    sprintf(gTopics.config_topic, MAX_LEN_TOPIC, "%s/%d/config/CONFIG", device, id);
+    sprintf(gTopics.delay_topic, MAX_LEN_TOPIC, "%s/%d/config/DELAY", device, id);
     sprintf(gTopics.telemetry_topic, MAX_LEN_TOPIC, "%s/%d/telemetry", device, id);
     
     /**
@@ -131,34 +131,31 @@ void mqtt_app_start(mqtt_callback callback, char device[MAX_LEN_DEVICE], int id)
     ESP_LOGI(TAG_MQTT,"APP MQTT START\n");
 }
 
-void publish_data(data_t* data)
-{
-    /*
-        Hay que crear una instancia de json_out para cada magnitud porque sino da fallo el compilador
-        Por como se define la jerarquita topica es:
-        <dispositivo>/<id>/telemetry/<magnitud-fisica>
-        
-        El payload:
-        {
-            id
-            valor
-            unidad
-        }
-    */
+eComm_err comm_send_telemetry(comm_telemetry_t* data){
     char buffer[128];
+
+    char temperature_topic[MAX_LEN_TOPIC];
+    char humidicity_topic[MAX_LEN_TOPIC];
+    char light_topic[MAX_LEN_TOPIC];
+
+    /**
+     * Se debe generar un topico por cada magnitud fisica segun viene especificado en la jerarquia topica
+     */
+    sprintf(temperature_topic, MAX_LEN_TOPIC, "%s/temperature", gTopics.telemetry_topic);
+    sprintf(humidicity_topic, MAX_LEN_TOPIC, "%s/humidicity", gTopics.telemetry_topic);
+    sprintf(light_topic, MAX_LEN_TOPIC, "%s/light", gTopics.telemetry_topic);
+
     struct json_out out_temperature = JSON_OUT_BUF(buffer, sizeof(buffer));
-    json_printf(&out_temperature, "{id: %d, temperature: %d, unidad: %s}", id_device, data->temperature, "Celsius");
-    
-    char topic_temperature[MAX_LEN_TOPIC];
-    sprintf(topic_temperature, MAX_LEN_TOPIC, "%s/temperature", gTopics.telemetry_topic);
-    
-    esp_mqtt_client_publish(client, "ESP32/1/telemetry/temperature", buffer, 0, 0, 0);
+    json_printf(&out_temperature, "{id: %d, temperature: %d, unidad: %s}", id_device, data->temperature, "Celsius");    
+    esp_mqtt_client_publish(client, temperature_topic, buffer, 0, 0, 0);
     
     struct json_out out_humidicity = JSON_OUT_BUF(buffer, sizeof(buffer));
     json_printf(&out_humidicity, "{id: %d, humidicity: %d, unidad: %s}", id_device, data->humicity, "percentage");
-    esp_mqtt_client_publish(client, "ESP32/1/telemetry/humidicity", buffer, 0, 0, 0);
+    esp_mqtt_client_publish(client, humidicity_topic, buffer, 0, 0, 0);
 
     struct json_out out_light = JSON_OUT_BUF(buffer, sizeof(buffer));
     json_printf(&out_light, "{id: %d, light: %d, unidad: %s}", id_device, data->light, "bool");
-    esp_mqtt_client_publish(client, "ESP32/1/telemetry/light", buffer, 0, 0, 0);
+    esp_mqtt_client_publish(client, light_topic, buffer, 0, 0, 0);
+
+    return COMM_OK;
 }
--- firmware/esp32-circuit/components/Communications/include/communications.h
+++ firmware/esp32-circuit/components/Communications/include/communications.h
@@ -3,7 +3,7 @@
 
 /**
  * @file communications.h
- * @brief Modulo de manejo de MQTT y gestion de comunicaciones
+ * @brief Modulo gestion de comunicaciones
  * @author Jose Manuel Enriquez Baena
  * @date 08-02-2026
  */
@@ -23,42 +23,36 @@ typedef struct{
     uint8_t humicity;
     uint8_t temperature;
     uint8_t light;
-}data_t;
+}comm_telemetry_t;
 
 /**
  * @brief Especifica los errores que ocurren en MQTT
  */
 typedef enum{
-    MQTT_OK,
-    MQTT_ERR_INVALID
-}eMQTT_err;
+    COMM_OK,
+    COMM_ERR_INVALID
+}eComm_err;
 
 /**
  *  @brief Topicos a los que el modulo esta suscrito
  */
 typedef enum{
-    MQTT_ON,
-    MQTT_SLEEP,
-    MQTT_CONFIG,
-    MQTT_DELAY
-}eMQTT_topic;
+    ON,
+    SLEEP,
+    CONFIG,
+    DELAY
+}eComm_message_type;
 
 /**
  *  @brief Estructura que define que tipo de mensaje que recibe por parametros la callback
  */
 typedef struct{
-    eMQTT_err status;
-    eMQTT_topic topic;
+    eComm_err status;
+    eComm_message_type message_type;
     char* data;
-}mqtt_message_t;
+}comm_message_t;
 
-typedef void(*mqtt_callback)(mqtt_message_t message);
-extern const char* TAG_MQTT;
-
-// Las variables se definen en un archivo privado por seguridad.
-extern const char* broker_uri;
-extern const char* username;
-extern const char* password;
+typedef void(*comm_callback)(comm_message_t message);
 
 /**
  * @brief Configuracion y conexion con el broker MQTT
@@ -66,7 +60,6 @@ extern const char* password;
  * @details Configura los parametros necesarios como el broker uri, credenciales, client_id para poder
  * iniciar cliente MQTT y registrar el manejor de eventos MQTT
  */
-void mqtt_app_start(mqtt_callback callback, char device[MAX_LEN_DEVICE], int id);
-void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
-void publish_data(data_t* data);
+void comm_init(comm_callback callback, char device[MAX_LEN_DEVICE], int id);
+eComm_err comm_send_telemetry(comm_telemetry_t* data);
 #endif
